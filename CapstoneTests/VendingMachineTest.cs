09c24429aa17636ea53fff3dcaf2001d12676b21
Sawyer Redpath <sawyerbruceredpath@gmail.com>
2018-10-12 22:35:18 -0400

    Finished working on test methods

--- CapstoneTests/VendingMachineTest.cs
+++ CapstoneTests/VendingMachineTest.cs
@@ -20,5 +20,99 @@ namespace CapstoneTests
 
             Assert.AreEqual(expectedString, consume);
         }
+
+        [DataTestMethod]
+        [DataRow("Test1", false)]
+        [DataRow("12345", true)]
+        [DataRow("Test", false)]
+        public void IsDigitsOnly_ReturnFalseOrTrue_Appropriately(string testString, bool expected)
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+
+            // Act
+            bool digitOnly = vm.IsDigitsOnly(testString);
+
+            // Assert
+            Assert.AreEqual(expected, digitOnly);
+        }
+
+        [TestMethod]
+        public void GiveChange_LeavesBalanceAt0 ()
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+            vm.Balance = 12M;
+
+            // Act
+            vm.GiveChange(vm);
+
+            // Assert
+            Assert.AreEqual(0M, vm.Balance);
+        }
+
+        [TestMethod]
+        public void VerifyProductCodeExists_ReturnFalseIfDoesNot()
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+
+
+            // Act
+            vm.StockVendingMachine("vendingmachine.csv");
+            bool exists = vm.verifyProductCodeExists("Z1");
+
+            // Assert
+            Assert.AreEqual(exists, false);
+
+        }
+
+        [TestMethod]
+        public void VerifyProductCodeExists_ReturnTrueIfDoes()
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+
+
+            // Act
+            vm.StockVendingMachine("vendingmachine.csv");
+            bool exists = vm.verifyProductCodeExists("A1");
+
+            // Assert
+            Assert.AreEqual(exists, true);
+
+        }
+
+        [TestMethod]
+        public void VerifyProductNotSoldOut_ReturnTrueIfNotSoldOut()
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+            vm.StockVendingMachine("vendingmachine.csv");
+
+
+            // Act
+            vm.CurrentStock["A1"].SlotStock = 5;
+            bool notSoldOut = vm.verifyProductNotSoldOut("A1");
+
+            // Assert
+            Assert.AreEqual(true, notSoldOut);
+        }
+
+        [TestMethod]
+        public void VerifyProductNotSoldOut_ReturnFalseIfSoldOut()
+        {
+            // Arrange
+            VendingMachine vm = new VendingMachine();
+            vm.StockVendingMachine("vendingmachine.csv");
+
+
+            // Act
+            vm.CurrentStock["A1"].SlotStock = 0;
+            bool notSoldOut = vm.verifyProductNotSoldOut("A1");
+
+            // Assert
+            Assert.AreEqual(false, notSoldOut);
+        }
     }
 }
